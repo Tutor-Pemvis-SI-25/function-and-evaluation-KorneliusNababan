c12bb68d3657ff6c6f0352fcb2ac6bc079c3b0e8
korneliusnababan66-wq <korneliusnababan66@gmail.com>
2025-11-18 23:24:16 +0700

    Selesai
    Selesai

--- TLAST.java
+++ TLAST.java
@@ -1,9 +1,8 @@
-//Kornelius Nababan - 12S25013
-
+// Kornelius Nababan - 12S25013
 import java.util.*;
 import java.lang.Math;
 
-public class TLAST {
+public class Program {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
