4a75980153170ced04f1a2272aad556d5dd85b0e
korneliusnababan66-wq <korneliusnababan66@gmail.com>
2025-11-18 23:33:23 +0700

    selesai
    Selesai

--- TLAST.java
+++ TLAST.java
@@ -1,8 +1,9 @@
 // Kornelius Nababan - 12S25013
+
 import java.util.*;
 import java.lang.Math;
 
-public class Program {
+public class TLAST {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
